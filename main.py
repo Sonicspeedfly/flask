529e658bfb4c0bfbd57ae53be9fe9349556ec251
Sonicspeedfly <70383270+Sonicspeedfly@users.noreply.github.com>
2024-07-13 18:28:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def webhook():
-    data = request.json
-    user_id = data['message']['from']['id']
-    first_name = data['message']['from']['first_name']
-    return jsonify({"user_id": user_id, "first_name": first_name})
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
-    
